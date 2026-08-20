46c573914d6640a44ce7cc66479effffea0c5cff
Haitam Zitouni <h_zitouni@salemstate.edu>
2026-08-20 22:36:32 +0100

    Add new Vercel URL to CORS allowed origins

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ client= OpenAI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173" , "https://ai-resume-generator-6xyzgswj1-haitam-zitouni.vercel.app"],
+    allow_origins=["http://localhost:5173" , "https://ai-resume-generator-bay-sigma.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
